0640487c6ed5ade23e267f9b6b5d8c42d8b06bac
SkylerRo <149194025+SkylerRo@users.noreply.github.com>
2023-11-30 17:05:12 -0500

    updatedfizzbuzz

--- index.js
+++ index.js
@@ -26,38 +26,34 @@ if (n % i == 0) {
 
 }
 
-// part 3 
+// Part 2: Prime Time
+// Now we will move onto something slightly more complex.
+// Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
+// Write a script that accomplishes the following:
 
-const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26/n"
+// prime bigger than 1, 
+// whole Number, 
+// when #/# = 1 and 
+// when #/1 = #
 
+// As soon as you find the prime number, log that number and exit the loop.
 
-let currentCell = 1
-let cell1 = '';
-let cell2 = '';
-let cell3 = '';
-let cell4 = '';
-
-for (let i = 0; i < csv.length; i++) {
-
-
-   
- if (csv[i] === ","){
-
- currentcell++   
-
-} else if (csv[i] === "\n") {
-
-console.log(cell1,cell2,cell3,cell4);
-currentCell = 1;
-cell1 = '';
-cell2 = '';
-cell3 = '';
-cell4 = '';
-
-} else {
-if (currentCell===1) {
-    
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+let num = 13;
+let primeNum = false;
+// 7 <= (13 / 2) = 6.5 -  while less than 6.5, keep going - stops at 7
+for (let count = 2; count <= num / 2; count++) {
+    // if 13 / 6 leaves a remainder of 0 - false because leaves remainder
+    if (num % count == 0) {
+        primeNum = true;
+        console.log(count + " is prime.")
+        break;
+    }
+        // add 1 to count
 }
-}  
+// if (primeNum = true)
+//     console.log(count + " is prime.")
+// Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+// Be careful! If you set n to a number too large, your loop could take a long time to process.
 
-}
